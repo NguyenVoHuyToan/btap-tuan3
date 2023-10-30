a41b2b96e78a5dfa9f3b9dd10dff41fd5cd680f5
NguyenVoHuyToan <huytoan9295@gmail.com>
2023-10-30 20:19:03 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Header from "./component/header/Header";
 import Button from "./component/Button";
-import Title from "./component/title/Title";
-import Footer from "./component/footer/Footer";
 import Content from "./component/content/Content";
+import Footer from "./component/footer/Footer";
+import Header from "./component/header/Header";
+import Title from "./component/title/Title";
 
 function App() {
   return (
